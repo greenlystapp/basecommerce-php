b636e053816688f0549b22d7e50ee594281dcb6d
abisheksrikaanth <abishek@technicaliti.me>
2019-09-21 12:33:27 +0530

    Adding dummy test to check if tests run successfully on travis

---
+++ tests/unit/ValidateCredentialsTest.php
@@ -0,0 +1,22 @@
+<?php 
+class ValidateCredentialsTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $this->assertTrue(true);
+    }
+}
